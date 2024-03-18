2fc2e7cc656fe71bd907fab80c84dd1101f3ad9a
giuliocorradini <39673120+giuliocorradini@users.noreply.github.com>
2024-03-18 15:39:09 +0100

    Implemented testpass as function analysis pass

--- llvm/lib/Transforms/Utils/TestPass.cpp
+++ llvm/lib/Transforms/Utils/TestPass.cpp
@@ -1,10 +1,31 @@
 #include "llvm/Transforms/Utils/TestPass.h"
 #include "llvm/IR/PassManager.h"
+#include "llvm/IR/Instructions.h"
 
 namespace llvm {
     PreservedAnalyses TestPass::run(Function &F, FunctionAnalysisManager &AM) {
         errs() << "Questa funzione si chiama " << F.getName() << "\n";
         errs() << "Il numero di argomenti è " << F.arg_end() - F.arg_begin() << "\n";
+        
+        int bbnum = 0;
+        int calls = 0;
+        int instructions = 0;
+
+        for (auto const &bb: F) {
+            bbnum++;
+            
+            for (auto &ins: bb) {
+                instructions++;
+
+                if (auto *CI = dyn_cast<CallInst>(&ins))
+                    calls++;
+            }
+        }
+
+        errs() << "Numero di chiamate a funzione " << calls << "\n";
+        errs() << "Numero di basic blocks " << bbnum << "\n";
+        errs() << "Numero di istruzioni " << instructions << "\n";
+
         return PreservedAnalyses::all();
     }
 }
