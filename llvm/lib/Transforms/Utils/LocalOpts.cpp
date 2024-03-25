97e183dc0adc2dddaf0a9b66fd16455818cbc769
giuliocorradini <39673120+giuliocorradini@users.noreply.github.com>
2024-03-25 15:45:37 +0100

    Slide 4 esercizio 2

--- llvm/lib/Transforms/Utils/LocalOpts.cpp
+++ llvm/lib/Transforms/Utils/LocalOpts.cpp
@@ -9,62 +9,59 @@
 #include "llvm/Transforms/Utils/LocalOpts.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/IR/InstrTypes.h"
+#include <string>
+#include <vector>
+using namespace std;
 
 using namespace llvm;
 
+/**
+ * Strength reduction of multiplication by power of 2s.
+ * Converts Muls into left shifts.
+*/
 bool runOnBasicBlock(BasicBlock &B) {
-    
-    // Preleviamo le prime due istruzioni del BB
-    Instruction &Inst1st = *B.begin(), &Inst2nd = *(++B.begin());
+  vector<Instruction *> toBeErased;
 
-    // L'indirizzo della prima istruzione deve essere uguale a quello del 
-    // primo operando della seconda istruzione (per costruzione dell'esempio)
-    assert(&Inst1st == Inst2nd.getOperand(0));
+  // For each instruction of 
+  for (auto &i: B) {
+    BinaryOperator *mul = dyn_cast<BinaryOperator>(&i);
+    if (not mul or mul->getOpcode() != BinaryOperator::Mul)
+      continue;
 
-    // Stampa la prima istruzione
-    outs() << "PRIMA ISTRUZIONE: " << Inst1st << "\n";
-    // Stampa la prima istruzione come operando
-    outs() << "COME OPERANDO: ";
-    Inst1st.printAsOperand(outs(), false);
-    outs() << "\n";
+    ConstantInt *C;
+    Value *Factor;
 
-    // User-->Use-->Value
-    outs() << "I MIEI OPERANDI SONO:\n";
-    for (auto *Iter = Inst1st.op_begin(); Iter != Inst1st.op_end(); ++Iter) {
-      Value *Operand = *Iter;
+    C = dyn_cast<ConstantInt>(mul->getOperand(0)); // Get first operand and try casting to constant
+    Factor = mul->getOperand(1);
 
-      if (Argument *Arg = dyn_cast<Argument>(Operand)) {
-        outs() << "\t" << *Arg << ": SONO L'ARGOMENTO N. " << Arg->getArgNo() 
-	       <<" DELLA FUNZIONE " << Arg->getParent()->getName()
-               << "\n";
+    if (not C or not C->getValue().isPowerOf2()) {
+      C = dyn_cast<ConstantInt>(mul->getOperand(1));
+
+      if (not C or not C->getValue().isPowerOf2()) {
+        outs() << *mul << ": no operand is a constant integer nor power of 2\n";
+        continue;
       }
-      if (ConstantInt *C = dyn_cast<ConstantInt>(Operand)) {
-        outs() << "\t" << *C << ": SONO UNA COSTANTE INTERA DI VALORE " << C->getValue()
-               << "\n";
-      }
+
+      Factor = mul->getOperand(0);
     }
 
-    outs() << "LA LISTA DEI MIEI USERS:\n";
-    for (auto Iter = Inst1st.user_begin(); Iter != Inst1st.user_end(); ++Iter) {
-      outs() << "\t" << *(dyn_cast<Instruction>(*Iter)) << "\n";
-    }
+    outs() << "Converting " << *mul << " to left shift\n";
+    Constant *shiftConst = ConstantInt::get(C->getType(), C->getValue().exactLogBase2());
+    Instruction *new_shift = BinaryOperator::Create(
+      BinaryOperator::Shl, Factor, shiftConst
+    );
 
-    outs() << "E DEI MIEI USI (CHE E' LA STESSA):\n";
-    for (auto Iter = Inst1st.use_begin(); Iter != Inst1st.use_end(); ++Iter) {
-      outs() << "\t" << *(dyn_cast<Instruction>(Iter->getUser())) << "\n";
-    }
+    new_shift->insertAfter(mul);
+    mul->replaceAllUsesWith(new_shift);
 
-    // Manipolazione delle istruzioni
-    Instruction *NewInst = BinaryOperator::Create(
-        Instruction::Add, Inst1st.getOperand(0), Inst1st.getOperand(0));
-
-    NewInst->insertAfter(&Inst1st);
-    // Si possono aggiornare le singole references separatamente?
-    // Controlla la documentazione e prova a rispondere.
-    Inst1st.replaceAllUsesWith(NewInst);
-
-    return true;
+    toBeErased.push_back(mul);
   }
+
+  for (Instruction *i: toBeErased)
+    i->eraseFromParent();
+
+  return true;
+}
 
 
 bool runOnFunction(Function &F) {
