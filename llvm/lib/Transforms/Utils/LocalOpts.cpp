6e68a582d8bcce3485410f52d62c6a160b7faa6a
giuliocorradini <39673120+giuliocorradini@users.noreply.github.com>
2024-03-28 16:25:52 +0100

    [AS1-3] Pass cleanup Description comment added at file beginning

--- llvm/lib/Transforms/Utils/LocalOpts.cpp
+++ llvm/lib/Transforms/Utils/LocalOpts.cpp
@@ -5,6 +5,11 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
+//
+// This pass implements algebraic reduction, strength reduction and
+// multi-instruction optimizations.
+//
+//===----------------------------------------------------------------------===//
 
 #include "llvm/Transforms/Utils/LocalOpts.h"
 #include "llvm/IR/Instructions.h"
@@ -15,51 +20,7 @@ using namespace std;
 
 using namespace llvm;
 
-/**
- * Strength reduction of multiplication by power of 2s.
- * Converts Muls into left shifts.
-*/
 bool runOnBasicBlock(BasicBlock &B) {
-  vector<Instruction *> toBeErased;
-
-  // For each instruction of 
-  for (auto &i: B) {
-    BinaryOperator *mul = dyn_cast<BinaryOperator>(&i);
-    if (not mul or mul->getOpcode() != BinaryOperator::Mul)
-      continue;
-
-    ConstantInt *C;
-    Value *Factor;
-
-    C = dyn_cast<ConstantInt>(mul->getOperand(0)); // Get first operand and try casting to constant
-    Factor = mul->getOperand(1);
-
-    if (not C or not C->getValue().isPowerOf2()) {
-      C = dyn_cast<ConstantInt>(mul->getOperand(1));
-
-      if (not C or not C->getValue().isPowerOf2()) {
-        outs() << *mul << ": no operand is a constant integer nor power of 2\n";
-        continue;
-      }
-
-      Factor = mul->getOperand(0);
-    }
-
-    outs() << "Converting " << *mul << " to left shift\n";
-    Constant *shiftConst = ConstantInt::get(C->getType(), C->getValue().exactLogBase2());
-    Instruction *new_shift = BinaryOperator::Create(
-      BinaryOperator::Shl, Factor, shiftConst
-    );
-
-    new_shift->insertAfter(mul);
-    mul->replaceAllUsesWith(new_shift);
-
-    toBeErased.push_back(mul);
-  }
-
-  for (Instruction *i: toBeErased)
-    i->eraseFromParent();
-
   return true;
 }
 
