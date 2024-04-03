a39317b2443013baa2f0ff3236eae87f7e2cc6aa
giuliocorradini <39673120+giuliocorradini@users.noreply.github.com>
2024-04-03 17:21:41 +0200

    [AS1-3] Optimize on function, reverse op gerarchy

--- llvm/lib/Transforms/Utils/LocalOpts.cpp
+++ llvm/lib/Transforms/Utils/LocalOpts.cpp
@@ -15,10 +15,75 @@
 #include "llvm/IR/Instructions.h"
 #include "llvm/IR/InstrTypes.h"
 #include <string>
-#include <vector>
+#include <map>
+#include <optional>
 using namespace std;
 
 using namespace llvm;
+
+namespace MultiInstructionOpt {
+  optional<Instruction::BinaryOps> getReverseOpcode(BinaryOperator *I) {
+    auto opcode = I->getOpcode();
+
+    static map<Instruction::BinaryOps, Instruction::BinaryOps> reverse = {
+      {Instruction::Add, Instruction::Sub},
+      {Instruction::Sub, Instruction::Add},
+      {Instruction::Mul, Instruction::SDiv},
+      {Instruction::SDiv, Instruction::Mul},
+      {Instruction::Shl, Instruction::LShr},
+      {Instruction::LShr, Instruction::Shl},
+      {Instruction::FNeg, Instruction::FNeg}
+    };
+
+    if (auto r = reverse.find(opcode); r != reverse.end())
+      return make_optional(r->second);
+
+    return nullopt;
+  }
+
+  /**
+   * Gets the other operand, given an instruction and one of the operands
+  */
+  Value *getOtherOperand(Instruction &I, Value *o) {
+    auto op = I.getOperand(0);
+    
+    return (op == o) ? I.getOperand(1) : op;
+  }
+
+  /**
+   * Confronts operands to see if the opposite operation is performed.
+  */
+  bool isReverseOperation(BinaryOperator *A, BinaryOperator *B) {
+    if (auto revOpcode = getReverseOpcode(A); revOpcode == B->getOpcode()) {
+      ;
+    }
+  }
+
+  bool optimizeOn(BasicBlock &B) {
+    for (Instruction &I: B) {
+      
+      auto BinaryOp = dyn_cast<BinaryOperator>(&I);
+      if (not BinaryOp) {
+        // Not a binary operator, won't optimize here
+        continue;
+      }
+
+      
+      for (auto user: I.users()) {
+        auto UserBinaryOp = dyn_cast<BinaryOperator>(&I);
+        if (not UserBinaryOp)
+          continue;
+
+        if (isReverseOperation(BinaryOp, UserBinaryOp)) {
+          UserBinaryOp->replaceAllUsesWith(BinaryOp);
+        }
+      }
+      
+    }
+
+    return true;
+  }
+}
 
 bool runOnBasicBlock(BasicBlock &B) {
   return true;
