602b7301a022dda9a7fec4f7f1205fdf1b1359a7
Emanuele Reggiani <manu@MacBook-Air-di-Emanuele.local>
2024-04-05 18:56:10 +0200

    [AS1-2] adding StrengthReduction for Mul and Div

--- llvm/lib/Transforms/Utils/LocalOpts.cpp
+++ llvm/lib/Transforms/Utils/LocalOpts.cpp
@@ -20,8 +20,99 @@ using namespace std;
 
 using namespace llvm;
 
+int isPowerOf2OrAdj(const APInt& value){
+	if (value.isPowerOf2())
+		return 0;
+	else if ((value + 1).isPowerOf2())
+		return 1;
+	else if ( (value - 1).isPowerOf2())
+		return -1;
+	return -2;
+}
+
+void strenghtReduction(Instruction &inst, std::vector<Instruction *> &eraseList){
+	if (inst.getOpcode() == BinaryOperator::Mul){
+		// prendo l'argomento 1 e 2
+		Value *fac1 = inst.getOperand(0);
+		Value *fac2 =  inst.getOperand(1);
+		if (ConstantInt *C = dyn_cast<ConstantInt>(fac1)) {
+			// Se il primo è una costante ed è potenza del 2
+			int distance = isPowerOf2OrAdj(C->getValue());
+			if(distance != -2){
+				const APInt& modifiedValue = C->getValue() + distance;
+				// Creo la costante di shift 
+				Constant *shift = ConstantInt::get(C->getType(), modifiedValue.logBase2());
+				// Creo una nuova istruzione e la genero dopo la moltiplicazione
+				Instruction* shiftInst = BinaryOperator::Create(Instruction::Shl, fac2, shift);
+				shiftInst->insertAfter(&inst);
+				if (distance == -1){
+					Instruction* secondOp = BinaryOperator::Create(Instruction::Sub, shiftInst, fac2);
+					secondOp->insertAfter(shiftInst);
+					inst.replaceAllUsesWith(secondOp);
+				}
+				else if (distance == 1){
+					Instruction* secondOp = BinaryOperator::Create(Instruction::Add, shiftInst, fac2);
+					secondOp->insertAfter(shiftInst);
+					inst.replaceAllUsesWith(secondOp);
+				}
+				else
+					inst.replaceAllUsesWith(shiftInst);
+				eraseList.push_back(&inst);
+			}
+		}
+		else if(ConstantInt *C = dyn_cast<ConstantInt>(fac2)){
+			int distance = isPowerOf2OrAdj(C->getValue());
+			if(distance != -2){
+				const APInt& modifiedValue = C->getValue() + distance;
+				// Creo la costante di shift 
+				Constant *shift = ConstantInt::get(C->getType(), modifiedValue.logBase2());
+				// Creo una nuova istruzione e la genero dopo la moltiplicazione
+				Instruction* shiftInst = BinaryOperator::Create(Instruction::Shl, fac1, shift);
+				shiftInst->insertAfter(&inst);
+				if (distance == -1){
+					Instruction* secondOp = BinaryOperator::Create(Instruction::Sub, shiftInst, fac1);
+					secondOp->insertAfter(shiftInst);
+					inst.replaceAllUsesWith(secondOp);
+				}
+				else if (distance == 1){
+					Instruction* secondOp = BinaryOperator::Create(Instruction::Add, shiftInst, fac1);
+					secondOp->insertAfter(shiftInst);
+					inst.replaceAllUsesWith(secondOp);
+				}
+				else
+					inst.replaceAllUsesWith(shiftInst);
+				eraseList.push_back(&inst);
+			}
+		}
+	}
+	//Se è una divisione
+	if (inst.getOpcode() == BinaryOperator::SDiv){
+		// prendo l'argomento 1 e 2
+		Value *fac1 = inst.getOperand(0);
+		Value *fac2 =  inst.getOperand(1);
+		if(ConstantInt *C = dyn_cast<ConstantInt>(fac2)){
+			if(C->getValue().isPowerOf2()){
+				// Creo la costante di shift 
+				Constant *shift = ConstantInt::get(C->getType(), C->getValue().logBase2());
+				// Creo una nuova istruzione e la genero dopo la divisione
+				Instruction* shiftInst = BinaryOperator::Create(Instruction::AShr, fac1, shift);
+				shiftInst->insertAfter(&inst);
+				inst.replaceAllUsesWith(shiftInst);
+				eraseList.push_back(&inst);
+			}
+		}
+	}
+}
+
 bool runOnBasicBlock(BasicBlock &B) {
-  return true;
+	std::vector<Instruction *> eraseList;
+	for (auto &inst : B){
+		strenghtReduction(inst, eraseList);
+	}
+	for (auto &inst : eraseList){
+		inst->eraseFromParent();
+	}
+	return true;
 }
 
 
@@ -39,11 +130,12 @@ bool runOnFunction(Function &F) {
 
 
 PreservedAnalyses LocalOpts::run(Module &M,
-                                      ModuleAnalysisManager &AM) {
+ModuleAnalysisManager &AM) {
   for (auto Fiter = M.begin(); Fiter != M.end(); ++Fiter)
     if (runOnFunction(*Fiter))
       return PreservedAnalyses::none();
   
   return PreservedAnalyses::all();
 }
+
 
