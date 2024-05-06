098a40327d344e98de8aac7588292baac20a74e9
giuliocorradini <39673120+giuliocorradini@users.noreply.github.com>
2024-05-06 15:07:26 +0200

    Fixed possible crash

--- llvm/lib/Transforms/Utils/LoopWalk.cpp
+++ llvm/lib/Transforms/Utils/LoopWalk.cpp
@@ -4,16 +4,19 @@
 PreservedAnalyses LoopWalkPass::run(Loop &L, LoopAnalysisManager &LAM, LoopStandardAnalysisResults &LAR, LPMUpdater &LU) {
     errs() << "This loop is " << (L.isLoopSimplifyForm() ? "" : "not ") << "in simplify form\n";
 
-    errs() << "The preheader: " << L.getLoopPreheader()->getName() << "\n";
-    errs() << "The header: " << L.getHeader()->getName() << "\n";
+    errs() << "The preheader: " << L.getLoopPreheader()->getNameOrAsOperand() << "\n";
+    errs() << "The header: " << L.getHeader()->getNameOrAsOperand() << "\n";
 
     errs() << "Basic blocks:\n";
 
     for (auto BB: L.getBlocks()) {
-        errs() << BB->getName() << "\n";
+        errs() << BB->getNameOrAsOperand() << "\n";
     }
 
-    errs() << "The exit block is: " << L.getExitBlock()->getName() << "\n";
+    if (L.getExitBlock())
+        errs() << "The exit block is: " << L.getExitBlock()->getNameOrAsOperand() << "\n";
+    else
+        errs() << "Multiple exit blocks\n";
 
     return PreservedAnalyses::all();
 }
