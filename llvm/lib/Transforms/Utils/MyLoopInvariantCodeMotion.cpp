b86aba5888e68e06fcd690aa259c2d19098464a7
Manu <emanuele.s.reggiani@gmail.com>
2024-05-13 16:55:01 +0200

    implementata la move

--- llvm/lib/Transforms/Utils/MyLoopInvariantCodeMotion.cpp
+++ llvm/lib/Transforms/Utils/MyLoopInvariantCodeMotion.cpp
@@ -1,9 +1,9 @@
 #include "llvm/IR/Argument.h"
 #include "llvm/IR/BasicBlock.h"
 #include "llvm/IR/Dominators.h"
+#include "llvm/IR/InstrTypes.h"
 #include "llvm/IR/Instruction.h"
 #include "llvm/IR/Instructions.h"
-#include "llvm/IR/InstrTypes.h"
 #include "llvm/IR/Metadata.h"
 #include "llvm/Pass.h"
 #include "llvm/Support/raw_ostream.h"
@@ -15,6 +15,8 @@ using namespace llvm;
 
 bool isLoopInvariant(Instruction &Inst, Loop &L);
 bool isLoopInvariant(Value *Operand, Loop &L);
+
+void moveToPreHeader(Instruction *Inst);
 
 void markInstruction(Instruction *Inst) {
     LLVMContext &C = Inst->getContext();
@@ -64,17 +66,43 @@ bool doesDominateAllBlocks(BasicBlock *A, SmallVectorImpl<BasicBlock *> &ExitBlo
     return true;
 }
 
-void loopOnBB(BasicBlock &BB, Loop &L, bool IsDominator){
+bool isDeadAfterLoop(Instruction &Inst, Loop &L){
+    for (auto *User : Inst.users()){
+        if (Instruction *I = dyn_cast<Instruction>(User); I && !L.contains(I))
+            return false;
+    }
+    return true;
+}
+
+void loopOnBB(BasicBlock &BB, Loop &L, bool IsDominator, std::vector<Instruction *> &Mov){
     for (auto &I : BB)
         if(isLoopInvariant(I, L)){
             outs() << "l'istruzione" << I << " è loop Invariant\n";
             //COntrollo la dominanza del blocco di appartenenza
-            if (IsDominator)
+            if (IsDominator || isDeadAfterLoop(I,L) ){// oppure la roba deigli usi
                 outs() << "è anche candidato per la move\n";
+                Mov.push_back(&I);
+            }
         }
+
+}
+
+void moveToPreHeader(Instruction *Inst, BasicBlock *PreHeader){
+    for (User::op_iterator Operand = Inst->op_begin(); Operand != Inst->op_end(); ++Operand) {
+        Value *OperandValue = *Operand;
+        if (dyn_cast<Constant>(OperandValue) || dyn_cast<Argument>(OperandValue))
+            continue;
+        moveToPreHeader(dyn_cast<Instruction>(OperandValue), PreHeader);
+    }
+    Inst->removeFromParent();
+    Inst->insertInto(PreHeader, --PreHeader->end());
+    //BasicBlock::iterator InsertPoint = --PreHeader->end();
+    //Inst->insertBefore(&*InsertPoint);
+    outs()<< "inserita l'isturzione " << Inst << " nel blocco: " << PreHeader << "\n";
 }
 
 PreservedAnalyses llvm::LICMyPass::run(Loop &L, LoopAnalysisManager &LAM, LoopStandardAnalysisResults &LAR, LPMUpdater &LU){
+    std::vector<Instruction *> MovableInst;
     if (!L.isLoopSimplifyForm()){
         return PreservedAnalyses::all();
     }
@@ -84,10 +112,13 @@ PreservedAnalyses llvm::LICMyPass::run(Loop &L, LoopAnalysisManager &LAM, LoopSt
     for(Loop::block_iterator BI = L.block_begin(); BI != L.block_end(); ++BI){
         BasicBlock *B = *BI;
         bool IsDom = doesDominateAllBlocks(B, ExitBlocks, DT);
-        loopOnBB(*B, L, IsDom);
+        loopOnBB(*B, L, IsDom, MovableInst);
     }
-
-
+    BasicBlock *PreHeader = L.getLoopPreheader();
+    outs() << "lunghezza Movable = " << MovableInst.size() << "\n";
+    for (Instruction *Inst : MovableInst){
+        moveToPreHeader(Inst, PreHeader);
+    }
 
     return PreservedAnalyses::all();
 }
