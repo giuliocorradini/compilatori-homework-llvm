d837c35e07b685bf98fa9ec4b95fd25fe2fc1ae1
Manu <emanuele.s.reggiani@gmail.com>
2024-05-24 12:25:26 +0200

    start loopfusepass

--- llvm/lib/Transforms/Utils/MyLoopFuse.cpp
+++ llvm/lib/Transforms/Utils/MyLoopFuse.cpp
@@ -1,14 +1,57 @@
 #include "llvm/Transforms/Utils/MyLoopFuse.h"
 #include "llvm/Analysis/LoopInfo.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Instruction.h"
 #include "llvm/IR/PassManager.h"
 
 using namespace llvm;
 
+llvm::BasicBlock* getFirstSuccessor(llvm::BasicBlock *BB) {
+    if (!BB->getTerminator()) {
+        return nullptr;
+    }
+    llvm::Instruction *Terminator = BB->getTerminator();
+    if (Terminator->getNumSuccessors() == 0) {
+        return nullptr;
+    }
+    return Terminator->getSuccessor(0);
+}
+
+bool areLoopAdj(Loop *L1, Loop *L2){
+    BasicBlock *HeadL2 = L2->getLoopPreheader();
+    if (L2->isGuarded())    //Controllo se L2 è guarded
+        HeadL2 = L2->getLoopGuardBranch()->getParent();
+    BasicBlock *ExitBlock1 = L1->getExitBlock();
+    outs()<< "L1 Exit: " << ExitBlock1<< "\n";
+    ExitBlock1->print(outs());
+    outs()<< "L2 Head: " << HeadL2<< "\n";
+    HeadL2->print(outs());
+    outs()<< "L2 real Head: " << L2->getHeader() << "\n";
+    L2->getHeader()->print(outs());
+    for (llvm::BasicBlock *Succ : successors(ExitBlock1)) {
+        if (Succ == HeadL2) {
+            return true;
+        }
+    }
+    return false;
+}
+
+
 PreservedAnalyses MyLoopFusePass::run(Function &F, FunctionAnalysisManager &AM) {
 
     LoopInfo &LI = AM.getResult<LoopAnalysis>(F);
+    Loop *OldLoop = nullptr;
     for (auto *L : LI){
-            outs() << L << "\n";
+        outs() << "L[i] = " << L <<"\n";
+        L->print(outs());
+        outs() << "LOLD = " << OldLoop << "\n";
+        OldLoop->print(outs());
+        if (OldLoop and OldLoop != L)
+            if(areLoopAdj(OldLoop, L)){
+                outs()<<"Il loop("<<*OldLoop<<") "<<OldLoop<<" è adiacente a";
+                outs()<<" loop("<<*L<<") "<<L<<"\n";
+            }
+        OldLoop = L;
     }
     return PreservedAnalyses::all();
 }
