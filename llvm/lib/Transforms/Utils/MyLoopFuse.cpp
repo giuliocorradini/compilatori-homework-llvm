03db4a0dbd17aac0fabf8d61ffed927d6d007d7a
Manu <emanuele.s.reggiani@gmail.com>
2024-05-27 15:08:05 +0200

    finished point 1 and point 3: check if 2 loops are adj and if L1 dominates L2

--- llvm/lib/Transforms/Utils/MyLoopFuse.cpp
+++ llvm/lib/Transforms/Utils/MyLoopFuse.cpp
@@ -1,9 +1,13 @@
 #include "llvm/Transforms/Utils/MyLoopFuse.h"
 #include "llvm/Analysis/LoopInfo.h"
+#include "llvm/Analysis/PostDominators.h"
 #include "llvm/CodeGen/ValueTypes.h"
 #include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Dominators.h"
 #include "llvm/IR/Instruction.h"
 #include "llvm/IR/PassManager.h"
+#include "llvm/Support/raw_ostream.h"
+#include <MacTypes.h>
 
 using namespace llvm;
 
@@ -23,32 +27,32 @@ bool areLoopAdj(Loop *L1, Loop *L2){
     if (L2->isGuarded())    //Controllo se L2 è guarded
         HeadL2 = L2->getLoopGuardBranch()->getParent();
     BasicBlock *ExitBlock1 = L1->getExitBlock();
-    /*
-    outs()<< "L1 Exit: " << ExitBlock1<< "\n";
-    ExitBlock1->print(outs());
-    outs()<< "L2 Head: " << HeadL2<< "\n";
-    HeadL2->print(outs());
-    outs()<< "L2 real Head: " << L2->getHeader() << "\n";
-    L2->getHeader()->print(outs());*/
     if(ExitBlock1 and ExitBlock1 == HeadL2)
         return true;
     return false;
+}
+
+bool L1DominatesL2(Function &F, FunctionAnalysisManager &AM, Loop *L1, Loop *L2){
+    DominatorTree &DT = AM.getResult<DominatorTreeAnalysis>(F);
+    PostDominatorTree &PDT = AM.getResult<PostDominatorTreeAnalysis>(F);
+    BasicBlock *H1 = L1->getHeader();
+    BasicBlock *H2 = L2->getHeader();
+    return DT.dominates(H1,H2) and PDT.dominates(H2, H1);
+
 }
 
 
 PreservedAnalyses MyLoopFusePass::run(Function &F, FunctionAnalysisManager &AM) {
 
     LoopInfo &LI = AM.getResult<LoopAnalysis>(F);
+
     Loop *OldLoop = nullptr;
-    for (auto *L : LI){
-        /*outs() << "L[i] = " << L <<"\n";
-        L->print(outs());
-        outs() << "LOLD = " << OldLoop << "\n";
-        if (OldLoop) OldLoop->print(outs());*/
+    for (auto *L : LI.getLoopsInReverseSiblingPreorder()){
         if (OldLoop and OldLoop != L)
-            if(areLoopAdj(L, OldLoop)){
+        if(areLoopAdj(L, OldLoop) && L1DominatesL2(F,AM, L, OldLoop)){
                 outs()<<"Il loop("<<OldLoop<<") "<<" è adiacente a";
                 outs()<<" loop("<<L<<")\n";
+                outs() << "E lo domina pure \n";
             }
         OldLoop = L;
     }
