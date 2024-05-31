fb0f2bc05c0e0de8760b0fc885c65326ba6dcc72
giuliocorradini <39673120+giuliocorradini@users.noreply.github.com>
2024-05-31 09:50:42 +0200

    [AS4] Dominance and same iterations

--- llvm/lib/Transforms/Utils/MyLoopFuse.cpp
+++ llvm/lib/Transforms/Utils/MyLoopFuse.cpp
@@ -6,8 +6,8 @@
 #include "llvm/IR/Dominators.h"
 #include "llvm/IR/Instruction.h"
 #include "llvm/IR/PassManager.h"
-#include "llvm/Support/raw_ostream.h"
-#include <MacTypes.h>
+#include "llvm/Analysis/ScalarEvolution.h"
+
 
 using namespace llvm;
 
@@ -37,23 +37,48 @@ bool L1DominatesL2(Function &F, FunctionAnalysisManager &AM, Loop *L1, Loop *L2)
     PostDominatorTree &PDT = AM.getResult<PostDominatorTreeAnalysis>(F);
     BasicBlock *H1 = L1->getHeader();
     BasicBlock *H2 = L2->getHeader();
-    return DT.dominates(H1,H2) and PDT.dominates(H2, H1);
+    errs() << "Dominanza: " << DT.dominates(H1, H2) << " reverse: " << PDT.dominates(H2, H1) << "\n";
 
+    return DT.dominates(H1,H2) and PDT.dominates(H2, H1);
+}
+
+bool iterateSameTimes(Function &F, FunctionAnalysisManager &AM, Loop *L1, Loop *L2) {
+    ScalarEvolution &SE = AM.getResult<ScalarEvolutionAnalysis>(F);
+
+    const SCEV *L1_execs = SE.getBackedgeTakenCount(L1);
+    const SCEV *L2_execs = SE.getBackedgeTakenCount(L2);
+
+    if (isa<SCEVCouldNotCompute>(L1_execs) or isa<SCEVCouldNotCompute>(L2_execs))
+        return false;
+
+    errs() << "L1 trip count ";
+    L1_execs->print(errs());
+    
+    errs() << "L2 trip count ";
+    L2_execs->print(errs());
+
+    
+    return SE.isKnownPredicate(ICmpInst::ICMP_EQ, L1_execs, L2_execs);
 }
 
 
 PreservedAnalyses MyLoopFusePass::run(Function &F, FunctionAnalysisManager &AM) {
 
+    errs() << "Welcome to loop fusion\n";
+
     LoopInfo &LI = AM.getResult<LoopAnalysis>(F);
 
     Loop *OldLoop = nullptr;
-    for (auto *L : LI.getLoopsInReverseSiblingPreorder()){
-        if (OldLoop and OldLoop != L)
-        if(areLoopAdj(L, OldLoop) && L1DominatesL2(F,AM, L, OldLoop)){
-                outs()<<"Il loop("<<OldLoop<<") "<<" è adiacente a";
-                outs()<<" loop("<<L<<")\n";
-                outs() << "E lo domina pure \n";
+    for (auto *L : reverse(LI)){
+        errs() << "Analizzo " << L->getLoopPreheader()->getNameOrAsOperand() << "\n";
+        if (OldLoop and OldLoop != L) {
+            if(areLoopAdj(OldLoop, L) && L1DominatesL2(F,AM, OldLoop, L) && iterateSameTimes(F, AM, OldLoop, L)){
+                errs()<<"Il loop("<<OldLoop<<") "<<" è adiacente a";
+                errs()<<" loop("<<L<<")\n";
+                errs() << "E lo domina pure \n";
+                errs() << "Hanno lo stesso number di iterazioni\n";
             }
+        }
         OldLoop = L;
     }
     return PreservedAnalyses::all();
