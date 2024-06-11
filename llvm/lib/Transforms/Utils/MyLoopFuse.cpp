c4ff3f37593f69ccbf67a6ed9adcc42d4975ebd6
giuliocorradini <39673120+giuliocorradini@users.noreply.github.com>
2024-06-11 21:05:10 +0200

    [AS-4] Init of negative dep check

--- llvm/lib/Transforms/Utils/MyLoopFuse.cpp
+++ llvm/lib/Transforms/Utils/MyLoopFuse.cpp
@@ -7,9 +7,11 @@
 #include "llvm/IR/Instruction.h"
 #include "llvm/IR/PassManager.h"
 #include "llvm/Analysis/ScalarEvolution.h"
-
+#include "llvm/Analysis/DependenceAnalysis.h"
+#include <set>
 
 using namespace llvm;
+using namespace std;
 
 llvm::BasicBlock* getFirstSuccessor(llvm::BasicBlock *BB) {
     if (!BB->getTerminator()) {
@@ -61,6 +63,51 @@ bool iterateSameTimes(Function &F, FunctionAnalysisManager &AM, Loop *L1, Loop *
     return SE.isKnownPredicate(ICmpInst::ICMP_EQ, L1_execs, L2_execs);
 }
 
+static bool checkNegativeDependencyForLoadAndSetOfStores(DependenceInfo &DI, LoadInst &L, set<StoreInst *> &stores) {
+    for (auto s: stores) {
+        auto dep = DI.depends(&L, s, false);
+        if (dep) {
+            errs() << "There is a dependency with store: " << s->getOperand(1)->getNameOrAsOperand() << " distance: " << dep->getDirection(dep->getLevels()) << "\n";
+            if (dep->isDirectionNegative())
+                return true;
+        }
+    }
+
+    return false;
+}
+
+static bool noNegativeDependencies(Function &F, FunctionAnalysisManager &AM, Loop &L1, Loop &L2) {
+    DependenceInfo &DI = AM.getResult<DependenceAnalysis>(F);
+
+    set<StoreInst *> l1_stores = {};
+    errs() << "Analyzing L1\n";
+
+    for (auto BB1: L1.blocks()) {
+        for (auto &I: *BB1) {
+            if (auto store = dyn_cast<StoreInst>(&I); store) {
+                l1_stores.insert(store);
+                errs() << "store: " << store->getOperand(1)->getNameOrAsOperand() << "\n";
+            }
+        }
+    }
+
+    errs() << "Analyzing L2\n";
+    for (auto BB2: L2.blocks()) {
+        errs() << "BB: " << BB2->getNameOrAsOperand() << "\n";
+
+        for (auto &I: *BB2) {
+            if (auto load = dyn_cast<LoadInst>(&I); load) {
+                errs() << "Found a load for " << load->getOperand(0)->getNameOrAsOperand() << "\n";
+                if (checkNegativeDependencyForLoadAndSetOfStores(DI, *load, l1_stores))
+                    return false;
+            }
+        }
+        
+    }
+
+    return true;
+}
+
 
 PreservedAnalyses MyLoopFusePass::run(Function &F, FunctionAnalysisManager &AM) {
 
@@ -70,7 +117,11 @@ PreservedAnalyses MyLoopFusePass::run(Function &F, FunctionAnalysisManager &AM) 
 
     Loop *OldLoop = nullptr;
     for (auto *L : reverse(LI)){
-        errs() << "Analizzo " << L->getLoopPreheader()->getNameOrAsOperand() << "\n";
+
+        errs() << "old: " << OldLoop << " new: " << L << "\n";
+        errs() << "Analizzo il loop " << L->getLoopPreheader()->getNameOrAsOperand() << "\n";
+        errs() << "è in simplify form? " << L->isLoopSimplifyForm() << "\n";
+        
         if (OldLoop and OldLoop != L) {
             if(areLoopAdj(OldLoop, L) && L1DominatesL2(F,AM, OldLoop, L) && iterateSameTimes(F, AM, OldLoop, L)){
                 errs()<<"Il loop("<<OldLoop<<") "<<" è adiacente a";
@@ -78,8 +129,12 @@ PreservedAnalyses MyLoopFusePass::run(Function &F, FunctionAnalysisManager &AM) 
                 errs() << "E lo domina pure \n";
                 errs() << "Hanno lo stesso number di iterazioni\n";
             }
+
+            bool negdep = noNegativeDependencies(F, AM, *OldLoop, *L);
+            errs() << "Neg dep results: " << negdep << "\n";
         }
         OldLoop = L;
+
     }
     return PreservedAnalyses::all();
 }
